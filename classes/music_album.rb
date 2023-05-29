3e27b417e5119b5e572cd5d478352758e214f55a
Progress2002 <ezeamakachisom2002@gmail.com>
2023-05-29 17:47:40 +0100

    Add can_be_archived? method in MusicAlbum class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -3,11 +3,15 @@ require_relative 'genre'
 
 class MusicAlbum < Item
      attr_reader :on_spotify, :publish_date, :genre
-     
+
     def initialize(on_spotify, publish_date, genre)
         super(publish_date: publish_date)
         @on_spotify = on_spotify
         @publish_date = publish_date
         @genre = genre
+    end
+
+    def can_be_archived?
+        super && on_spotify == true
     end
 end
