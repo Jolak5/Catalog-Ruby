0634be99af1378e1a49f5a8077c1a122a47a4f22
Progress2002 <ezeamakachisom2002@gmail.com>
2023-05-29 17:39:25 +0100

    Create Genre class and add add_item method

---
+++ classes/genre.rb
@@ -0,0 +1,17 @@
+class Genre 
+    attr_reader :id
+    attr_accessor :name, : items
+
+    def initialize(name)
+        super()
+        @id = Random.rand(1..1000)
+        @name = name
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item
+        item.genre = self
+        @items << item
+    end
+end
