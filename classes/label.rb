2e6bec6bdcb0a74b912ccec1083f4af464a9992f
Enis <enis.mema@gmail.com>
2023-05-30 11:39:27 +0200

    Fix linters

--- classes/label.rb
+++ classes/label.rb
@@ -1,14 +1,15 @@
 class Label
-    attr_reader :id, :title, :color, :items
-    def initialize(title, color)
+  attr_reader :id, :title, :color, :items
+
+  def initialize(title, color)
     @id = Random.rand(1...1000)
     @title = title
     @color = color
     @items = []
-    end
+  end
 
-    def add_item(item)
-        @items << item
-        item.add_label(self)
-    end
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
 end
