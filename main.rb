078c5dd24f5045c1dc569617120ce08e7265411e
Progress2002 <ezeamakachisom2002@gmail.com>
2023-05-30 11:01:16 +0100

    Update app UI

--- main.rb
+++ main.rb
@@ -4,16 +4,16 @@ class Main
   puts 'Welcome to our catalog app'
   def select_option
     puts 'Please choose an option by entering a number:'
-    puts '1  - List all books'
-    puts '2  - List all music albums'
-    puts '3  - List all games'
-    puts '4  - List all genres'
-    puts '5  - List all labels'
-    puts '6  - List all authors'
-    puts '7  - Add a book'
-    puts '8  - Add a music album'
-    puts '9  - Add a game'
-    puts '10 - exit'
+    puts '1). List all books'
+    puts '2). List all music albums'
+    puts '3). List all games'
+    puts '4). List all genres'
+    puts '5). List all labels'
+    puts '6). List all authors'
+    puts '7). Add a book'
+    puts '8). Add a music album'
+    puts '9). Add a game'
+    puts '10). exit'
   end
 
   def start
@@ -21,10 +21,13 @@ class Main
     loop do
       select_option
       option = gets.chomp
-      break if option == '10'
-
-      new_app.select_option(option)
-      new_app.add_items(option)
+      if option == '10'
+        puts 'Thank you for using this app '
+        break
+      else
+        new_app.select_option(option)
+        new_app.add_items(option)
+      end
     end
   end
 end
